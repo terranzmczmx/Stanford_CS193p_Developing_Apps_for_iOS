02fcd8cace7023b8e84763d3b1c5b1b62adca661
Yingzhe Hu <wing@Wings-M1-Max.attlocal.net>
2024-06-18 17:20:52 -0700

    project basic setup

---
+++ assignment_1_memorize/Assignment 1 Memorize/Assignment 1 Memorize/Assignment_1_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_1_MemorizeApp.swift
+//  Assignment 1 Memorize
+//
+//  Created by Yingzhe Hu on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_1_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ assignment_1_memorize/Assignment 1 Memorize/Assignment 1 Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Assignment 1 Memorize
+//
+//  Created by Yingzhe Hu on 6/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
